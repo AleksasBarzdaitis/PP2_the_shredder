a933045952250a635cf4b00ac0954c897d6f596e
Aleksas <a.barzdaitis@gmail.com>
2022-07-03 18:24:35 +0300

    Added config file and ability to select picture and shred size

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import numpy as np
+import yaml
 from matplotlib import image
-import operations as op
+import shredder_actions as sa
 
-# Loading image
-img = image.imread('images\dog1.jpg')
+# Loading image from config file
+with open('.\config\config.yml', 'r') as config:
+    options = yaml.safe_load(config)['options']
+    loaded_image = options['images']
+img = image.imread(loaded_image)
+
 print(f'Original image shape: {img.shape}')
 
 # Rounding image size to tens
@@ -16,11 +21,9 @@ rounded_img_width = (original_img_width // 10) * 10
 eligible_divisors = False
 while not eligible_divisors:
     common_divisors = []
-
     for i in range(10, min(rounded_img_height, rounded_img_width), 2):
         if rounded_img_height % i == 0 and rounded_img_width % i == 0:
             common_divisors.append(i)
-
     if common_divisors[-1] < 30:
         rounded_img_height -= 10
     else:
@@ -29,8 +32,20 @@ while not eligible_divisors:
 # Cropping image to determined size
 cropped_img = img[0:rounded_img_height, 0:rounded_img_width]
 
-# Setting up shredding frequency
-shred_freq = common_divisors[-1]
+# Setting up The Shredder
+# Loading config file to get size of shreds (shredded and glued image quality)
+with open('.\config\config.yml', 'r') as config:
+    options = yaml.safe_load(config)['options']
+    quality_selected = options['display_quality']
+
+print(f'Shredded image quality selected: {quality_selected}')
+
+if quality_selected == 'low':
+    shred_freq = common_divisors[0]
+elif quality_selected == 'medium':
+    shred_freq = common_divisors[len(common_divisors) // 2]
+elif quality_selected == 'high':
+    shred_freq = common_divisors[-1]
 
 print(f'Cropped image shape: {cropped_img.shape}')
 print(f'Common divisors: {common_divisors}')
@@ -41,15 +56,15 @@ titles = ['Original image', 'Cropped image',
           'Final result of shredding, splitting and gluing']
 
 # Display original size and cropped image
-op.display_image(img, titles[0], 10)
-op.display_image(cropped_img, titles[1], 10)
+sa.display_image(img, titles[0], 5)
+sa.display_image(cropped_img, titles[1], 5)
 
-# Shredding, splitting, gluing and displaying cropped image
-first_shred = op.shredding_splitting_gluing(cropped_img, shred_freq, 1, np.hsplit)
-op.display_image(first_shred, titles[2], 10)
+# Shredding vertically, splitting, gluing and displaying cropped image
+first_shred = sa.shredding_splitting_gluing(cropped_img, shred_freq, 1, np.hsplit)
+sa.display_image(first_shred, titles[2], 10)
 
-# Shredding, splitting, gluing and displaying glued image
-second_shred = op.shredding_splitting_gluing(first_shred, shred_freq, 0, np.vsplit)
-op.display_image(second_shred, titles[3], 10)
+# Shredding horizontally, splitting, gluing and displaying glued image
+second_shred = sa.shredding_splitting_gluing(first_shred, shred_freq, 0, np.vsplit)
+sa.display_image(second_shred, titles[3], 10)
 
-print(f'Final shape after shredding: {second_shred.shape}')
+print(f'Final shape after shredding and gluing: {second_shred.shape}')
--- src/shredder_actions.py
+++ src/shredder_actions.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def shredding_splitting_gluing(img, shred_freq, axis, func):
     # Creating a list of vertically or horizontally shredded image pieces and converting it to numpy array
     list_of_shredded_pieces = func(img, shred_freq)
@@ -13,22 +14,19 @@ def shredding_splitting_gluing(img, shred_freq, axis, func):
             even_indexes.append(i)
         else:
             odd_indexes.append(i)
-    # Splitting image pieces to two
+    # Splitting shredded image pieces to two
     image1_pieces = arr[even_indexes]
     image2_pieces = arr[odd_indexes]
-    # "Gluing" pieces into two images
+    # "Gluing" split pieces into two images
     image1 = np.concatenate(image1_pieces, axis=axis)
     image2 = np.concatenate(image2_pieces, axis=axis)
-    # "Gluing" two images into one and displaying it
+    # "Gluing" two images into one
     image1_image2 = np.concatenate((image1, image2), axis=1)
-
     return image1_image2
 
 
 def display_image(img, title, pause):
     plt.imshow(img)
     plt.title(title)
-    # mng = plt.get_current_fig_manager()
-    # mng.full_screen_toggle()
     plt.show(block=False)
     plt.pause(pause)
